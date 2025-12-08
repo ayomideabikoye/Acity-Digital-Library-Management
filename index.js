c23ee9ddf4759f72fe7f9d5eb9474d2f2acef988
Ayomide_Abikoye <ayomide.abikoye@acity.edu.gh>
2025-12-08 16:55:19 +0000

    updating cors for github pages

--- index.js
+++ index.js
@@ -8,25 +8,15 @@ dotenv.config();
 const server = express();
 const PORT = process.env.PORT || 3000;
 
-const allowedOrigins = [
-    'https://ayomideabikoye.github.io',
-    'https://ayomideabikoye.github.io/Acity-Digital-Library-Frontend/' 
-];
 const authRoutes = require('./Routes/auth');
 const bookRoutes = require('./Routes/books');   
 const transactionRoutes = require('./Routes/transactions');
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-};
+const frontendURL = 'https://yourusername.github.io/your-repo-name'; 
 
+app.use(cors({
+    origin: frontendURL,
+}));
 server.use(cors(corsOptions));
 server.use(express.json()); 
 
