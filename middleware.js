18a51b7556f5235e5eed5cfc54bbee275efc001e
Ayomide_Abikoye <ayomide.abikoye@acity.edu.gh>
2025-12-09 00:13:56 +0000

    importing jwt

--- middleware.js
+++ middleware.js
@@ -1,10 +1,12 @@
+const jwt = require('jsonwebtoken');
+
 function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
     
     if (!token) return res.status(401).json({error: 'Authentication required'});
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => { 
         if (err) return res.status(403).json({error: 'Invalid token'});
         req.user = user;
         next();
